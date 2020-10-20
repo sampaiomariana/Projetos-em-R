ec1b07a2071c6eda4357db6336483a7ffe49b042
Mariana Sampaio <43734115+sampaiomariana@users.noreply.github.com>
2020-10-20 09:14:26 -0300

    Update ColetaTwitter.R

--- ColetaTwitter.R
+++ ColetaTwitter.R
@@ -14,19 +14,19 @@ library(RSQLite)
 
 #Validação dos objetos 
 
-api_key <- "cgReO4KR4CoKTFV66PM0wRLqF"
-api_key_secret <- "UaubaOtnTqZ4jyCZULt7vkh9MCEiR0IxVqPXoKD0hkdMHe5iSM"
-token_acesso <- "1228324028382949376-UrS2fjCveaqmLRNhSaH6RDUKwZqVjI"
-token_acesso_secreto <- "dMO78Mn6kiASAvmX6fhyHaZHfkzOJruauOJKWdj5h3bit"
+api_key <- ""
+api_key_secret <- ""
+token_acesso <- ""
+token_acesso_secreto <- ""
 
 #Autenticacão
 
 setup_twitter_oauth(api_key, api_key_secret, token_acesso,token_acesso_secreto)
 
-searchTwitteR("AEB")
-AEB <- searchTwitter("AEB", lang = "pt", n = 2500)
-AEB <- AEB %>% twListToDF()
-view(AEB)
+searchTwitteR("A")
+A <- searchTwitter("A", lang = "pt", n = 2500)
+A <- A %>% twListToDF()
+view(A)
 trends <- availableTrendLocations()
 head(trends)
 view(trends)
@@ -37,58 +37,13 @@ view(Brasilia)
 
 #Busca por hashtag
 
-AEB_TAG <- searchTwitter("#AEB", lang = "pt", n = 2500)
-AEB_TAG <- AEB_TAG %>% twListToDF()
-view(AEB_TAG)
+A_TAG <- searchTwitter("#covid", lang = "pt", n = 2500)
+A_TAG <- A_TAG %>% twListToDF()
+view(A_TAG)
 
 SPACE_X <- searchTwitter("#SpaceX", lang = "pt", n = 2500)
 SPACE_X <- SPACE_X %>% twListToDF()
 view(SPACE_X)
-
-#Tweets da conta da @espacial_aeb
-
-tweets_aeb <- searchTwitter("@espacial_aeb", n = 200)
-head(tweets_aeb)
-
-#Informacoes do usuArio
-
-myuser <- getUser("espacial_aeb")
-myuser$name
-myuser$getDescription()
-myuser$getCreated()
-myuser$followersCount
-myuser$favoritesCount
-myuser$friendsCount
-myuser$id
-myuser$lastStatus
-myuser$location
-
-
-#Inforamcoes direto da conta da @espacial_aeb
-
-timeline <- userTimeline("espacial_aeb", n = 100, maxID = NULL, sinceID = NULL,
-                         includeRts = TRUE, excludeReplies = FALSE)
-head(timeline)
-timeline <- timeline %>% twListToDF()
-view(timeline)
-str(timeline)
-summary(timeline)
-timeline$text
-
-
-#Acesso aos dados do retweeters
-#Post sobre : Encomenda Tecnológica (ETEC) é um instrumento...
-retweeters(id = "1268866631742230530", n = 20)
-resultado <- showStatus("1268866631742230530")
-retweeters(resultado$getId())
-
-#Chamamento Público AEB | Saiba mais sobre a importância
-retweeters(id = "1268599125702033409")
-resultado2 <- showStatus("1268599125702033409")
-retweeters(resultado2$getId())
-retweeters(resultado2$getIsRetweet())
-retweeters(resultado2$getLatitude())
-retweeters(resultado2$getLongitude())
 
 #Dados que são puxados da minha conta 
 #Minha timeline
@@ -151,23 +106,4 @@ ggplot(data = timeline1,mapping = aes(isRetweet,favoriteCount))+
 write.csv(timeline1,"timeline1.csv")
 write.table(timeline1,"timeline1.txt", sep = "\t")
 
-#Novos Dados
 
-userTimeline("espacial_aeb", n = 100 , maxID = NULL, sinceID = NULL,
-             includeRts = TRUE, excludeReplies = FALSE)
-mentions(n = 25, "espacial_aeb")
-user_aeb <- getUser ("espacial_aeb")
-print(user_aeb)
-user_aeb$id
-user_aeb$favoritesCount
-user_aeb$followersCount
-user_aeb$followRequestSent
-user_aeb$lang
-user_aeb$lastStatus
-user_aeb$initialize()
-user_aeb$location
-user_aeb$statusesCount
-user_aeb$verified
-
-users_aeb <- lookupUsers("espacial_aeb")
-head(users_aeb)
