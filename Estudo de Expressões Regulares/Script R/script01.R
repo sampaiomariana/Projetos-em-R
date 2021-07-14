e700f8338a86b46a4e2be218dc6c9828c6d4af37
mariana <marisampa26@gmail.com>
2021-07-14 12:07:52 -0300

    update script e relatório

--- Estudo de Expressões Regulares/Script R/script01.R
+++ Estudo de Expressões Regulares/Script R/script01.R
@@ -40,4 +40,40 @@ grupo02 %>% str_remove_all("jogador")
 grupo02 %>% str_remove("cantora")
 grupo02 %>% str_remove("Thiago")
 
+#Replace/Substituir 
+
+grupo03 <- c("Alcântara", "Alcantara", "lançamentos","lançamento","avançar","avanços","avançou","avanço")
+grupo03
+
+grupo03 %>% str_replace_all("lançamentos", "lançamento")
+grupo03 %>% str_replace_all("avanços|avançou|avançar", "avanço")
+
+#REGEX
+
+# \\s (espaço)
+grupo_check <- c("A AEB","A AEB","A AEB")
+grupo_check
+grupo04 <- c("  A AEB", "A   AEB  ", "  A      AEB")
+grupo04
+identical(grupo_check,grupo04)
+grupo04 <- grupo04 %>% str_replace_all("\\s{2,}", " ") %>% 
+  str_trim
+grupo04
+identical(grupo_check,grupo04)
+
+
+# \\D -> não dígito e \\d -> dígitos 0-9
+
+grupo05 <- c("AEB 25 anos", "AEB 26 anos", "AEB 27 anos")
+grupo05 %>% str_remove_all("\\d")
+grupo05 %>% str_remove_all("\\D") %>% 
+  as.numeric() %>%
+  mean
+
+# . -> qualquer coisa
+grupo06 <- c(" 5.55", "25 anos AEB.")
+grupo06
+grupo06 %>% str_subset("\\D\\.") # \\D -> não dígito
+grupo06 %>% str_subset("\\d\\.") # \\d -> dígito
+grupo06 %>% str_subset("\\.") # \\D -> não dígito
 
